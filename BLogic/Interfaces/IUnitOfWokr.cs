dc325f90c2facf7fd55c04d7f07c0a3a54e34d2c
Yulia2120 <ket-dz@rambler.ru>
2022-10-17 17:03:12 +0300

    UnitOfWork

--- BLogic/Interfaces/IUnitOfWokr.cs
+++ BLogic/Interfaces/IUnitOfWokr.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BLogic.Interfaces
+﻿namespace BLogic.Interfaces
 {
     public interface IUnitOfWokr
     {
